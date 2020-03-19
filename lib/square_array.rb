ea3bd1cc8814183df5556bc8c6626c716ec7bf88
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-19 15:34:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   result = []
   index = 0
   while (index < array.length) do
-    result << array[index]
+    result << (array[index] ** 2)
     index = index + 1
   end
   result
